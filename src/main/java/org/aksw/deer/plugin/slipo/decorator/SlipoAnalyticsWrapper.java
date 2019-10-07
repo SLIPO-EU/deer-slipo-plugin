5c3a0a1c4921a41fe2de591f91fc50cff82ee949
Kevin Dreßler <kvndrsslr@gmail.com>
2019-10-07 07:06:23 +0200

    new global stats

--- src/main/java/org/aksw/deer/plugin/slipo/decorator/SlipoAnalyticsWrapper.java
+++ src/main/java/org/aksw/deer/plugin/slipo/decorator/SlipoAnalyticsWrapper.java
@@ -4,35 +4,99 @@ import org.aksw.deer.DeerAnalyticsStore;
 import org.aksw.deer.DeerExecutionNode;
 import org.aksw.deer.ParameterizedDeerExecutionNode;
 import org.aksw.deer.decorators.AbstractDeerExecutionNodeDecorator;
-import org.aksw.deer.decorators.AbstractDeerExecutionNodeWrapper;
 import org.aksw.deer.decorators.AbstractParameterizedDeerExecutionNodeDecorator;
-import org.aksw.deer.decorators.SparqlAnalyticsWrapper;
+import org.aksw.deer.decorators.AbstractParameterizedDeerExecutionNodeWrapper;
 import org.aksw.faraday_cage.engine.ExecutionNode;
 import org.aksw.faraday_cage.engine.FaradayCageContext;
 import org.aksw.faraday_cage.engine.Parameterized;
+import org.aksw.faraday_cage.engine.ValidatableParameterMap;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
 import org.apache.jena.rdf.model.*;
 import org.json.JSONObject;
 import org.pf4j.Extension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  *
  */
 @Extension
-public class SlipoAnalyticsWrapper extends AbstractDeerExecutionNodeWrapper {
+public class SlipoAnalyticsWrapper extends AbstractParameterizedDeerExecutionNodeWrapper {
 
   private static final String SLIPO_NS = "http://slipo.eu/def#";
 
-  private static final Logger logger = LoggerFactory.getLogger(SparqlAnalyticsWrapper.class);
+  public static final Property GLOBAL_INPUT_ID= ResourceFactory.createProperty(SLIPO_NS + "globalInputId");
+
+  public static final Property GLOBAL_OUTPUT_ID= ResourceFactory.createProperty(SLIPO_NS + "globalOutputId");
+
+  private static final Logger logger = LoggerFactory.getLogger(SlipoAnalyticsWrapper.class);
+
+  private static final Map<String, Model> inputPerJob = new HashMap<>();
+
+  private static final Property category = ResourceFactory.createProperty(SLIPO_NS + "category");
+
+  private synchronized void addGlobalIn(Model in) {
+    inputPerJob.put(FaradayCageContext.getRunId(), in);
+  }
+
+  private void addGlobalOut(Model out) {
+    Model in = inputPerJob.get(FaradayCageContext.getRunId());
+
+    Set<Resource> inPOIs = in.listStatements(null, category, (RDFNode) null).mapWith(Statement::getSubject).toSet();
+    long avgDiff = 0;
+    long maxDiff = 0;
+    long enrichedPOIs = 0;
+    for (Resource inPOI : inPOIs) {
+      Query query = QueryFactory.create(
+        "prefix x: <urn:ex:>\n" +
+          "\n" +
+          "select (count(?o) AS ?cnt) \n" +
+          "where {\n" +
+          "  <" + inPOI.getURI() + "> (x:|!x:)* ?s .\n" +
+          "  ?s ?p ?o .\n" +
+          "}");
+      QueryExecution queryExecution = QueryExecutionFactory.create(query, in);
+      long inProperties = queryExecution.execSelect().next().get("?cnt").asLiteral().getLong();
+      queryExecution.close();
+      queryExecution = QueryExecutionFactory.create(query, out);
+      long outProperties = queryExecution.execSelect().next().get("?cnt").asLiteral().getLong();
+      queryExecution.close();
+      long diff = outProperties - inProperties;
+      avgDiff += diff;
+      if (diff > 0) {
+        enrichedPOIs++;
+        if (diff > maxDiff) {
+          maxDiff = diff;
+        }
+      }
+      in.listStatements(inPOI, null, (RDFNode) null).toList().size();
+    }
+    JSONObject root = new JSONObject();
+    // compute avg Delta of enriched POIs
+    root.put("enrichmentPercentage", 100d/in.size()*out.size()-100d);
+    root.put("totalEnrichedTriples", avgDiff);
+    root.put("avgEnrichedTriples", Math.round((double) avgDiff / (double) inPOIs.size()));
+    // compute max Delta of enriched POIs
+    root.put("maxEnrichedTriples", maxDiff);
+    // compute number of total POIs
+    root.put("totalPOIs", inPOIs.size());
+    // compute number of enriched POIs
+    root.put("enrichedPOIs", enrichedPOIs);
+    DeerAnalyticsStore.write(FaradayCageContext.getRunId(), null, root);
+    // clear reference
+    inputPerJob.put(FaradayCageContext.getRunId(), null);
+  }
 
   private void addStats(List<Model> in, String key, Resource id) {
-    final Property category = ResourceFactory.createProperty(SLIPO_NS + "category");
     final List<Double> triples = new ArrayList<>(in.size());
     final List<Long> pois = new ArrayList<>(in.size());
+    final List<Long> slipo = new ArrayList<>(in.size());
     for (Model m : in) {
       long poiCount = m.listStatements(null, category, (RDFNode) null).toModel().size();
       pois.add(poiCount);
@@ -42,11 +106,21 @@ public class SlipoAnalyticsWrapper extends AbstractDeerExecutionNodeWrapper {
       } else {
         triples.add(0d);
       }
+      AtomicLong slipoCounter = new AtomicLong(0);
+      m.listStatements()
+        .filterKeep(stmt -> stmt.getPredicate().getURI().startsWith(SLIPO_NS))
+        .forEachRemaining(stmt -> slipoCounter.incrementAndGet());
+      slipo.add(slipoCounter.get());
     }
-    JSONObject stats = new JSONObject();
-    stats.put("number of POIs in " + key, pois);
-    stats.put("triples per POI in " + key, triples);
-    DeerAnalyticsStore.write(FaradayCageContext.getRunId(), id, stats);
+    if (pois != null && pois.size() > 0 && !(pois.size() == 1 && pois.get(0) == 0)) {
+      JSONObject root = new JSONObject();
+      JSONObject group = new JSONObject();
+      root.put(key + "Stats", group);
+      group.put("numberPOIs", pois);
+      group.put("avgTriples", triples);
+      group.put("slipoProperties", slipo);
+      DeerAnalyticsStore.write(FaradayCageContext.getRunId(), id, root);
+    }
   }
 
   @Override
@@ -58,6 +132,14 @@ public class SlipoAnalyticsWrapper extends AbstractDeerExecutionNodeWrapper {
     }
   }
 
+  @Override
+  public ValidatableParameterMap createParameterMap() {
+    return ValidatableParameterMap.builder()
+      .declareProperty(GLOBAL_INPUT_ID)
+      .declareProperty(GLOBAL_OUTPUT_ID)
+      .build();
+  }
+
   private class SlipoAnalyticsDecorator extends AbstractDeerExecutionNodeDecorator {
 
     public SlipoAnalyticsDecorator(ExecutionNode<Model> other) {
@@ -66,8 +148,15 @@ public class SlipoAnalyticsWrapper extends AbstractDeerExecutionNodeWrapper {
 
     public List<Model> apply(List<Model> in) {
       List<Model> out = super.apply(in);
-      addStats(in, "input", getId());
-      addStats(out, "output", getId());
+      String globalIn = SlipoAnalyticsWrapper.this.getParameterMap().getOptional(GLOBAL_INPUT_ID).map(RDFNode::asResource).map(Resource::getURI).orElse("");
+      String globalOut = SlipoAnalyticsWrapper.this.getParameterMap().getOptional(GLOBAL_OUTPUT_ID).map(RDFNode::asResource).map(Resource::getURI).orElse("");
+      if (globalIn.equals(getId().getURI())) {
+        addGlobalIn(out.get(0));
+      } else if (globalOut.equals(getId().getURI())) {
+        addGlobalOut(in.get(0));
+      }
+      addStats(in, "slipoInput", getId());
+      addStats(out, "slipoOutput", getId());
       return out;
     }
 
@@ -81,15 +170,17 @@ public class SlipoAnalyticsWrapper extends AbstractDeerExecutionNodeWrapper {
 
     public List<Model> apply(List<Model> in) {
       List<Model> out = super.apply(in);
-      if (in.size() > 0) {
-        addStats(in, "input", getId());
+      String globalIn = SlipoAnalyticsWrapper.this.getParameterMap().getOptional(GLOBAL_INPUT_ID).map(RDFNode::asResource).map(Resource::getURI).orElse("");
+      String globalOut = SlipoAnalyticsWrapper.this.getParameterMap().getOptional(GLOBAL_OUTPUT_ID).map(RDFNode::asResource).map(Resource::getURI).orElse("");
+      if (globalIn.equals(getId().getURI())) {
+        addGlobalIn(out.get(0));
+      } else if (globalOut.equals(getId().getURI())) {
+        addGlobalOut(in.get(0));
       }
-      if (out.size() > 0) {
-        addStats(out, "output", getId());
-      }
+      addStats(in, "slipoInput", getId());
+      addStats(out, "slipoOutput", getId());
       return out;
     }
-
   }
 
   @Override
